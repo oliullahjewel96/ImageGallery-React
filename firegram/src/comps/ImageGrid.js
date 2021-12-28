1702f42f4b92e969928b0b285b723057a86b700a
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-28 21:39:52 +0600

    Created a modal to show the full picture

--- firegram/src/comps/ImageGrid.js
+++ firegram/src/comps/ImageGrid.js
@@ -1,15 +1,17 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
 
-const ImageGrid = () => {
+const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore("images");
-
-  console.log(docs);
   return (
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div className="img-wrap" key={doc.id}>
+          <div
+            className="img-wrap"
+            onClick={() => setSelectedImg(doc.url)}
+            key={doc.id}
+          >
             <img src={doc.url} alt="Images" />
           </div>
         ))}
