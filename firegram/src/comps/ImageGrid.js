b2b9fff9c6c499575c32e96b8e96f357c21ac56d
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-28 22:37:39 +0600

    Added animation using framer-motion

--- firegram/src/comps/ImageGrid.js
+++ firegram/src/comps/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
+import { motion } from "framer-motion/dist/framer-motion";
 
 const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore("images");
@@ -7,13 +8,21 @@ const ImageGrid = ({ setSelectedImg }) => {
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div
+          <motion.div
+            layout
             className="img-wrap"
             onClick={() => setSelectedImg(doc.url)}
             key={doc.id}
+            whileHover={{ opacity: 1 }}
           >
-            <img src={doc.url} alt="Images" />
-          </div>
+            <motion.img
+              src={doc.url}
+              alt="Images"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+          </motion.div>
         ))}
     </div>
   );
